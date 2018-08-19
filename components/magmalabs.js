e1531277b58ddcd30c9a4cac5ab312e71e8c57c7
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2018-08-19 01:53:50 -0500

    Move magma link to its own file

---
+++ components/magmalabs.js
@@ -0,0 +1,20 @@
+
+const MagmaLabs = () => (
+  <div>
+    <a href="https://www.magmalabs.io/" target="_blank" rel="noopener noreferrer" alt="MagmaLabs site">
+      <img className="logo-magma" src={require('../images/logo_magma.svg')} alt="MagmaLabs logo" />
+    </a>
+    <style jsx>
+      {`
+        div {
+            display: inline;
+        }
+        img {
+          height: 1em;
+        }
+      `}
+    </style>
+  </div>
+);
+
+export default MagmaLabs;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,5 @@
 import MainLayout from '../layouts/main';
-
-const MagmaLabs = () => (
-  <div>
-    <a href="https://www.magmalabs.io/" target="_blank" rel="noopener noreferrer" alt="MagmaLabs site">
-      <img className="logo-magma" src={require('../images/logo_magma.svg')} alt="MagmaLabs logo" />
-    </a>
-    <style jsx>
-      {`
-        div {
-            display: inline;
-        }
-        img {
-          height: 1em;
-        }
-      `}
-    </style>
-  </div>
-);
+import MagmaLabs from '../components/magmalabs';
 
 const Index = () => (
   <MainLayout>
