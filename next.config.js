2a676491d18541db7847f54b627779d6c91a4b82
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2018-08-19 00:39:04 -0500

    Add export page configuration

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
-const withImages = require('next-images')
-module.exports = withImages()
+const compose = require('compose-function');
+const withImages = require('next-images');
+
+const withExportedPages = require('./plugins/with-export-pages');
+
+module.exports = compose(
+  withExportedPages(),
+);
+
+module.exports = compose(
+  withImages,
+  withExportedPages(),
+)({
+  pageExtensions: ['js', 'mdx'],
+});
